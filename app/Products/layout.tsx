bed33a5780204fbb83cd78a00e5f79d77844d15e
HoangCongMin <0782708546hoangcongminh@gmail.com>
2023-06-23 21:42:20 +0700

    oktesst

---
+++ app/Products/layout.tsx
@@ -0,0 +1,37 @@
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ClientOnly from "@/components/ClientOly";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: {
+    default: "my - title",
+    template: "%s | my-title",
+  },
+  description: {
+    template: "%s",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+
+      <body>
+        <ClientOnly>
+
+        <Header />
+        {children}
+        <Footer />
+        </ClientOnly>
+      </body>
+    </html>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ScrollToTop from "@/components/ScrollToTop";
+import "node_modules/react-modal-video/css/modal-video.css";
+import "../styles/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Inter } from "next/font/google";
+import ClientOnly from "@/components/ClientOly";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const queryClient = new QueryClient();
+
+export const metadata = {
+  title: "3i-Sofware",
+  description: "Được thành lâp vào tháng 3 năm 2006 với tiền thân là một đội chuyên gia phần mềm và giải pháp viễn thông có kinh nghiệm từ Hàn Quốc. Chúng tôi đang từng bước đạt được uy tín và niềm tin từ khách hàng với những sản phẩm công nghệ hiện đại và phù hợp với các doanh nghiệp trong nước và các đối tác nước ngoài như Hàn Quốc , Nhật Bản , Trung Quốc.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+
+      <body className="dark:bg-black">
+        {/* <QueryClientProvider client={queryClient}> */}
+        <ClientOnly>
+
+          <Providers>
+            <Header />
+            {children}
+            <Footer />
+            <ScrollToTop />
+          </Providers>
+        </ClientOnly>
+        {/* </QueryClientProvider> */}
+      </body>
+    </html>
+  );
+}
+
+import { Providers } from "./providers";
